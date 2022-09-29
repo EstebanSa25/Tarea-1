cd340c70679388a6314f32c0e9e4729e07433668
Esteban Salas <110070428+EstebanSa25@users.noreply.github.com>
2022-09-29 15:12:35 -0600

    Update ArreglosUH.java

--- src/arreglosuh/ArreglosUH.java
+++ src/arreglosuh/ArreglosUH.java
@@ -10,38 +10,10 @@ public class ArreglosUH {
 
  
     public static void main(String[] args) {
-        int num1=5;
-        float flotante =24.33f;//32 bits
-        double doble =64.23d;//64 btis
-        boolean boleana=true;
-        byte b=0;//8 bits
-        short s=36;//16 bits
-        long l=455;
-        String cadena="Hoy es un excelente dia";
-        char caracter='c';
-   //System.out.println(num1);
-   // System.out.println(cadena.length());
-    int [] edad=new int[num1];
-    
-   int[] notas=new int[3];
-   String [] estudiantes=new String[]{"Maria","Marta","Jesus","Pedro"};
-   notas[0]=100;
-   notas[1]=70;
-   notas[2]=90;
-   
-   float sumatoria=0;
    int cont=1;
    int total=0;
    int totalInvertido=0;
-      //  for (int i = estudiantes.length-1; i >= 0; i--) {
-       //     System.out.println("Esdiantes: " +estudiantes[i]);
-        //}
-  
-        //  for (int i = 0; i < notas.length; i++) {
-           // sumatoria+=notas[i];
-       // }
-       //   sumatoria/=notas.length;
-         // System.out.println("promedio: "+sumatoria);
+    
           int[][]matriz=new int[3][3];
           for (int f = 0; f < 3; f++) {
               for (int c = 0; c < 3; c++) {
@@ -61,7 +33,7 @@ public class ArreglosUH {
               
               System.out.println("");
         }
-        // System.out.println(total); 
+        System.out.println(total); 
          
           System.out.println("Diagonal Principal");
          for (int F = 0; F < 3; F++) {
@@ -73,15 +45,19 @@ public class ArreglosUH {
              }
              System.out.print("\t");
         }
+          int totalInvertido2=0;
          System.out.println("\nDiagonal Secundaria");
         for (int F = 0; F < 3; F++) {
              for (int C = 0; C < 3; C++){
                  if ((F + C) == (3 - 1)) {
                     System.out.print("["+matriz[F][C] + "]");
+                  totalInvertido2+=matriz[F][C];
                 }
              }
              System.out.print("\t");
         }
+        int totalFinal=totalInvertido2+totalInvertido;
+        System.out.println("\nSuma de diagonal secundaria: "+totalInvertido2+"\nSuma de Diagonal Principal: "+totalInvertido+"\nTotal de suma de las diagonales: "+totalFinal);
     }//main
 
 }//class
